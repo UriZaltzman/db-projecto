eed103ab87d61ab9d8e826b59c228dde87d8287b
Pepe08tn <48521743@est.ort.edu.ar>
2024-08-19 23:37:28 -0300

    Tabla de los perfiles creada

--- Registro.js
+++ Registro.js
@@ -4,8 +4,8 @@ import client from "./dbconfig.js";
 const AddUser = async (req, res) => {
     try {
         const resul = await client.query(
-            'INSERT INTO Usuario2 (nombre, apellido) VALUES ($1, $2) RETURNING *',
-            [req.body.nombre, req.body.apellido]);
+            'INSERT INTO perfil (nombre, apellido, nick_name, mail, contrasena, saldo_cuenta, direccion, dni, nlv_uso_tecno) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+            [req.body.nombre, req.body.apellido, req.body.nick_name, req.body.mail, req.body.contrasena, req.body.saldo_cuenta, req.body.direccion, req.body.dni, req.body.nlv_uso_tecno]);
         res.json({ id: resul.insertId })
     }
     catch (err) {
