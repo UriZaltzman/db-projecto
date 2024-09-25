baec1bfe046a68a21c71853d1dbacb3f54b8adf8
Pepe08tn <48521743@est.ort.edu.ar>
2024-09-25 10:11:28 -0300

    Mas arreglos

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-import express from "express";
-import cors from "cors"; 
-// const bodyParser = require('body-parser');
-//import bodyParser from "body-parser";
-//const walletController = require('./controllers/wallet.js');
-import walletController from "./controllers/wallet.js"
-
-//app.use(bodyParser.json());
+import express from 'express';
+import cors from 'cors';
+import Registro from './controllers/Registro.js';
+import Usuario from './controllers/Usuario.js';
+import { verifyToken } from './middlewares/Usuario.middleware.js';
+// import { getSaldo, transferirDinero, getTransacciones } from './controllers/wallet.js';
 
 const app = express();
 const port = 3000;
 
-import Registro from "./controllers/Registro.js";
-import Usuario from "./controllers/Usuario.js";
-import { verifyToken } from "./middlewares/Usuario.middleware.js";
-
-// Corregido: Invocar correctamente el middleware de CORS
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -26,11 +20,12 @@ app.get("/", (_, res) => {
 app.post("/nuevo", Registro.AddUser);
 // Logearse
 app.post("/login", Usuario.Logearse);
-app.get("/profile/:id",verifyToken, Usuario.Profile);
+app.get("/profile/:id", verifyToken, Usuario.Profile);
 
-//app.use("/saldo", getSaldo);
-//app.use("/transferir", transferirDinero);
-//app.use("/transacciones", getTransacciones);
+// Uncomment these if needed
+// app.use("/saldo", getSaldo);
+// app.use("/transferir", transferirDinero);
+// app.use("/transacciones", getTransacciones);
 
 app.listen(port, () => {
     console.log(`Proyecto API listening at http://localhost:${port}`);
